b34a1324ed95610141ff11dd6f6895db243c26dc
Chonlasith Jucksriporn <chonlasith@gmail.com>
2023-12-10 11:45:32 +0700

    checkin: Add external js plugin

--- app/javascript/controllers/post_notice_controller.js
+++ app/javascript/controllers/post_notice_controller.js
@@ -1,8 +1,13 @@
 import { Controller } from '@hotwired/stimulus';
+import Toastify from 'toastify-js';
 
 export default class extends Controller {
     connect() { // auto run when created
         console.log('connected');
+        Toastify({
+            text: "This is a toast",
+            duration: 3000
+        }).showToast();
     }
 
     close() {
